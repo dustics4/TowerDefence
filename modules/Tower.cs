06a4e42b6fc9182ca8ba3d4243b791aea5de7266
dustics4 <konrad.babowicz21@gmail.com>
2024-12-01 21:58:57 +0000

    drawing line with cursor

--- modules/Tower.cs
+++ modules/Tower.cs
@@ -1,13 +1,20 @@
 using Godot;
 using System;
 using System.Numerics;
+using System.Collections.Generic;
 
 [Tool]
 public partial class Tower : Node2D
 {
+	public Godot.Vector2 Point1 {get; set;} = new Godot.Vector2(0f,0f);
+	public int Width {get; set;} = 10;
+	public Color Color {get; set;} = Colors.Green;
+
+	private Godot.Vector2 _point2;
+
     public override void _Draw()
     {
-		
+		DrawLine(Point1, _point2, Color, Width);
     }
 	
     public override void _Ready()  // Called when the node enters the scene tree for the first time.
@@ -18,7 +25,11 @@ public partial class Tower : Node2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-	
+		Godot.Vector2 mousePosition = GetViewport().GetMousePosition();
+		if(mousePosition != _point2){
+			_point2 = mousePosition;
+			QueueRedraw();
+		}
 
 	}
 }

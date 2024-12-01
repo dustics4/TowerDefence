2eb1cecc0927642646c1cde080105b32922db152
dustics4 <konrad.babowicz21@gmail.com>
2024-12-01 20:29:56 +0000

    Creating a base for our shapes

--- modules/Tower.cs
+++ modules/Tower.cs
@@ -4,17 +4,26 @@ using System.Numerics;
 
 public partial class Tower : Node2D
 {
-    // Called when the node enters the scene tree for the first time.
-
+	private float[,] _coordsHead = 
+	{
+		{ 22.952f, 83.271f },  { 28.385f, 98.623f },
+        { 53.168f, 107.647f }, { 72.998f, 107.647f },
+        { 99.546f, 98.623f },  { 105.048f, 83.271f },
+        { 105.029f, 55.237f }, { 110.740f, 47.082f },
+        { 102.364f, 36.104f }, { 94.050f, 40.940f },
+        { 85.189f, 34.445f },  { 85.963f, 24.194f },
+        { 73.507f, 19.930f },  { 68.883f, 28.936f },
+        { 59.118f, 28.936f },  { 54.494f, 19.930f },
+        { 42.039f, 24.194f },  { 42.814f, 34.445f },
+        { 33.951f, 40.940f },  { 25.637f, 36.104f },
+        { 17.262f, 47.082f },  { 22.973f, 55.237f }
+	};
     public override void _Draw()
     {
-		DrawLine(new Godot.Vector2(1.5f, 1.0f), new Godot.Vector2(1.5f, 4.0f), Colors.Green, 1.0f);
-		DrawLine(new Godot.Vector2(4.0f, 1.0f), new Godot.Vector2(4.0f, 4.0f), Colors.Green, 3.0f);
-		DrawLine(new Godot.Vector2(7.5f, 1.0f), new Godot.Vector2(7.0f, 4.0f),Colors.Green, 3.0f );
-		DrawRect(new Rect2(16.0f, 2.0f, 3.0f, 3.0f), Colors.Green, false, 2.0f);
-        base._Draw();
+		
     }
-    public override void _Ready()
+	
+    public override void _Ready()  // Called when the node enters the scene tree for the first time.
 	{
 	}
 

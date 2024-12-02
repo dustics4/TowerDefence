9ffc8f486134d2a3d55372aec7260f8c35cced55
dustics4 <konrad.babowicz21@gmail.com>
2024-12-02 21:43:45 +0000

    removed code

--- modules/Tower.cs
+++ modules/Tower.cs
@@ -6,15 +6,8 @@ using System.Collections.Generic;
 [Tool]
 public partial class Tower : Node2D
 {
-	public Godot.Vector2 Point1 {get; set;} = new Godot.Vector2(0f,0f);
-	public int Width {get; set;} = 10;
-	public Color Color {get; set;} = Colors.Green;
-
-	private Godot.Vector2 _point2;
-
     public override void _Draw()
     {
-		DrawLine(Point1, _point2, Color, Width);
     }
 	
     public override void _Ready()  // Called when the node enters the scene tree for the first time.
@@ -25,11 +18,7 @@ public partial class Tower : Node2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		Godot.Vector2 mousePosition = GetViewport().GetMousePosition();
-		if(mousePosition != _point2){
-			_point2 = mousePosition;
-			QueueRedraw();
-		}
+		QueueRedraw();
 
 	}
 }

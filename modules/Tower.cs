65325acaf15cb54b1bbf47d94d037f55a71298b0
dustics4 <konrad.babowicz21@gmail.com>
2024-12-02 22:32:42 +0000

    added collision and a shape

--- modules/Tower.cs
+++ modules/Tower.cs
@@ -1,18 +1,21 @@
 using Godot;
 using System;
-using System.Numerics;
 using System.Collections.Generic;
+using System.Reflection.Metadata;
 
 [Tool]
 public partial class Tower : Node2D
 {
+
+	
     public override void _Draw()
     {
+
     }
 	
     public override void _Ready()  // Called when the node enters the scene tree for the first time.
 	{
-		
+		GD.Print("Hello World");	
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.

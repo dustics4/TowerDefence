784adb4946c74aec816230d53a78a10c8e9678d6
dustics4 <konrad.babowicz21@gmail.com>
2024-12-03 15:46:28 +0000

    Can move now

--- modules/Test.cs
+++ modules/Test.cs
@@ -1,18 +1,64 @@
 using Godot;
 using System;
 
-public partial class Test : Area2D
+public partial class Test : CharacterBody2D
 {	
     [Export]
 	public int Speed {get; set;} = 200;
 	public Vector2 ScreenSize;
+
+
 	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
+	
+	public void GetInput()
 	{
+		Vector2 inputDirection = Input.GetVector("left", "right", "up", "down");
+		Velocity = inputDirection * Speed;
+	}
+
+    public override void _PhysicsProcess(double delta)
+    {
+        GetInput();
+		MoveAndSlide();
+    }
+    public override void _Ready()
+	{
+		ScreenSize = GetViewportRect().Size;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		var velocity = Vector2.Zero; // The player's movement vector.
+
+    if (Input.IsActionPressed("move_right"))
+    {
+        velocity.X += 1;
+    }
+
+    if (Input.IsActionPressed("move_left"))
+    {
+        velocity.X -= 1;
+    }
+
+    if (Input.IsActionPressed("move_down"))
+    {
+        velocity.Y += 1;
+    }
+
+    if (Input.IsActionPressed("move_up"))
+    {
+        velocity.Y -= 1;
+    }
+
+    if (velocity.Length() > 0)
+    {
+        velocity = velocity.Normalized() * Speed;
+    }
+	Position += velocity * (float)delta;
+	Position = new Vector2(
+		x: Mathf.Clamp(Position.X, 0, ScreenSize.X),
+		y: Mathf.Clamp(Position.Y, 0, ScreenSize.Y)
+	);
 	}
 }

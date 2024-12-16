4e2094d87b756ea9b1d17db3ce79ec43a0d6fe91
dustics4 <konrad.babowicz21@gmail.com>
2024-12-16 21:33:08 +0000

    Now able to reset test object. Unable to setup health label

--- modules/Test.cs
+++ modules/Test.cs
@@ -7,20 +7,30 @@ public partial class Test : CharacterBody2D
 	public delegate void HitEventHandler();
 	
 
-    [Export]
+	[Export]
 	public int Speed {get; set;} = 200;
 	
 	public Vector2 _initialPosition;
 	public Vector2 _targetPosition;
 	public Vector2 _screenSize;
+	[Export]
+	public Label _healthLabel;
 
-	public Area2D _tower;	
+	public Area2D _tower;
 
 	// Called when the node enters the scene tree for the first time.
-    
-    private void OnBodyEntered(Node2D body)
+	
+	private int _health = 100;
+	private void OnBodyEntered(Node2D body)
 	{
 		if(body == this){
+			_health -= 10;
+			if(_healthLabel != null){
+				_healthLabel.Text = $"Health : {_health}";
+				
+			}else{
+				GD.Print($"Health : {_health}");
+			}
 			Hide();
 			EmitSignal(SignalName.Hit);
 			GD.Print("Test collided with tower");
@@ -28,23 +38,23 @@ public partial class Test : CharacterBody2D
 	}
 
 
-    public override void _Ready()
+	public override void _Ready()
 	{
 		_screenSize = GetViewportRect().Size;
 
 		_tower = GetParent().GetNode<Area2D>("Tower");
 		if (_tower == null)
-        {
-            GD.PrintErr("Tower node not found!");
-        }
+		{
+			GD.PrintErr("Tower node not found!");
+		}
 		if(_tower != null){
 			_tower.BodyEntered += OnBodyEntered;
 		} 
 	}
 
-    public override void _PhysicsProcess(double delta)
-    {
-        if(_tower == null){
+	public override void _PhysicsProcess(double delta)
+	{
+		if(_tower == null){
 			return;
 		}
 
@@ -52,50 +62,50 @@ public partial class Test : CharacterBody2D
 		Velocity = (_targetPosition - GlobalPosition).Normalized() * Speed;
 
 		MoveAndSlide();
-    }
+	}
 
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(double delta)
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
 	{
 		var velocity = Vector2.Zero; // The player's movement vector.
 
-    if (Input.IsActionPressed("move_right"))
-    {
-        velocity.X += 1;
-    }
+		if (Input.IsActionPressed("move_right"))
+		{
+			velocity.X += 1;
+		}
 
-    if (Input.IsActionPressed("move_left"))
-    {
-        velocity.X -= 1;
-    }
+		if (Input.IsActionPressed("move_left"))
+		{
+			velocity.X -= 1;
+		}
 
-    if (Input.IsActionPressed("move_down"))
-    {
-        velocity.Y += 1;
-    }
+		if (Input.IsActionPressed("move_down"))
+		{
+			velocity.Y += 1;
+		}
 
-    if (Input.IsActionPressed("move_up"))
-    {
-        velocity.Y -= 1;
-    }
+		if (Input.IsActionPressed("move_up"))
+		{
+			velocity.Y -= 1;
+		}
 
-	if (Input.IsActionPressed("reset"))
-    {
-    	GD.Print("Resetting Test object position");
-		GlobalPosition = new Vector2(100,200);
-		Velocity = Vector2.Zero;
-    }
+		if (Input.IsActionPressed("reset"))
+		{
+			GD.Print("Resetting Test object position");
+			GlobalPosition = new Vector2(10,20);
+			Velocity = Vector2.Zero;
+		}
 
 
-    if (velocity.Length() > 0)
-    {
-        velocity = velocity.Normalized() * Speed;
-    }
-	Position += velocity * (float)delta;
+		if (velocity.Length() > 0)
+		{
+			velocity = velocity.Normalized() * Speed;
+		}
+		Position += velocity * (float)delta;
 
-	Position = new Vector2(
-		x: Mathf.Clamp(Position.X, 0, _screenSize.X),
-		y: Mathf.Clamp(Position.Y, 0, _screenSize.Y)
-	);
+		Position = new Vector2(
+			x: Mathf.Clamp(Position.X, 0, _screenSize.X),
+			y: Mathf.Clamp(Position.Y, 0, _screenSize.Y)
+		);
 	}
 }

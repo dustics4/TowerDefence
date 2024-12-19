06f9c75c8596be8b977bf26eb44acc484c89b0a2
dustics4 <konrad.babowicz21@gmail.com>
2024-12-19 19:42:40 +0000

    creating enemyspawner class manager

---
+++ modules/EnemySpawner.cs
@@ -0,0 +1,14 @@
+using Godot;
+using System;
+
+public class EnemySpawner
+{   
+    // pass enemy , within class to add into enemyspawner
+    
+    public EnemySpawner()
+    {
+        
+    }
+
+    
+}
--- modules/Main.cs
+++ modules/Main.cs
@@ -2,7 +2,9 @@ using Godot;
 using System;
 
 public partial class Main : Node
-{
+{	
+	private EnemySpawner _enemySpawner = new EnemySpawner();
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -12,4 +14,7 @@ public partial class Main : Node
 	public override void _Process(double delta)
 	{
 	}
+
+	/// 
+
 }

1b0fe0d2b9f0efea961c9152401537b7bd402249
dustics4 <konrad.babowicz21@gmail.com>
2024-12-23 18:59:47 +0000

    Changed gravity scale. learning how to use enum and creating constructor class within Enemy

--- modules/Enemy.cs
+++ modules/Enemy.cs
@@ -1,22 +1,27 @@
 using Godot;
 using System;
 
-public partial class Main : Node
-{	
-	private EnemySpawner _enemySpawner = new EnemySpawner();
-	private Tower tower = new Tower();
+public partial class Enemy : RigidBody2D
+{
+	public int _CurrentHealth;
+	public int _MaxHealth;
+	public enum _EnemyType 
+	{
+		enemy1,
+		enemy2,
+		enemy3
+		
+	};
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		_EnemyType enemyType = new _EnemyType{};
+
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-
 	}
-
-	/// 
-
 }
--- modules/EnemySpawner.cs
+++ modules/EnemySpawner.cs
@@ -4,11 +4,14 @@ using System;
 public class EnemySpawner
 {   
     // pass enemy , within class to add into enemyspawner
-    
+    public Enemy[] enemy; // creates an array of enemy
+    // colide with other enemies
+    // movement enemy
+    // what happens when enemy hits tower.
     public EnemySpawner()
     {
         
+        
     }
 
-    
 }

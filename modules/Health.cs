d604a5cc44d96610d38a06cd4cf392c93c2e8d77
dustics4 <konrad.babowicz21@gmail.com>
2025-01-09 21:50:28 +0000

    Removed ready and process, as that isn't needed.

--- modules/Health.cs
+++ modules/Health.cs
@@ -17,14 +17,4 @@ public partial class Health : Node
     {
 
     }
-
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-	}
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
 }

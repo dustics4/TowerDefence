d2a0f2c4e464aab4a3c877ebca1598b7acb21b99
dustics4 <konrad.babowicz21@gmail.com>
2025-01-09 21:50:03 +0000

    Created health constructor to manage health for different types of objects

--- modules/Health.cs
+++ modules/Health.cs
@@ -3,6 +3,21 @@ using System;
 
 public partial class Health : Node
 {
+    public int CurrentHealth;
+    public int MaxHealth;
+    public float Damage;
+    public Health()
+    {
+        this.MaxHealth = 100;
+        this.CurrentHealth = this.MaxHealth;
+        this.Damage = 10f;
+    }
+
+    public void TakeDamage()
+    {
+
+    }
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
